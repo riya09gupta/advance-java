a54c1b4ae247de3cf6ff15d2566927a8212754a9
riya09gupta <48875373+riya09gupta@users.noreply.github.com>
2019-05-10 13:05:29 +0530

    Add files via upload

---
+++ hellow/src/newjava/ADjava.java
@@ -0,0 +1,27 @@
+package newjava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ADjava
+ */
+@WebServlet("/ADjava")
+public class ADjava extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Welcome to service!</h1>");
+	}
+
+}
