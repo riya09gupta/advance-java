c8211045b7e0bd33fcf279b102dd5c29d013421f
riya09gupta <48875373+riya09gupta@users.noreply.github.com>
2019-05-12 15:45:49 +0530

    Add files via upload

---
+++ projectonTag/WebContent/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="tags" uri="WEB-INF/tags.tld" %>
+    <%@page import ="java.util.Date" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<tags:print-date/>
+
+</body>
+</html>
---
+++ projectonTag/src/projectonTag/DateTag.java
@@ -0,0 +1,17 @@
+package projectonTag;
+
+import java.io.IOException;
+import java.util.Date;
+
+//import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class DateTag extends SimpleTagSupport {
+	public void doTag() throws JspException, IOException {
+		JspWriter out = getJspContext().getOut();
+		out.println(new Date().toString());
+	}
+
+}
